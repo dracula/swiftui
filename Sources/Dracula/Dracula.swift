bc8436d48bf51f0f08ebfb71e968517049e6dd40
Raphael Cerqueira <raphaelfc@gmail.com>
2021-04-14 19:42:06 -0300

    Extension visibility

--- Sources/Dracula/Dracula.swift
+++ Sources/Dracula/Dracula.swift
@@ -1,15 +1,15 @@
 import SwiftUI
 
-extension Color {
-    public static let draculaBackground = Color("background")
-    public static let draculaCurrent = Color("current")
-    public static let draculaForeground = Color("foreground")
-    public static let draculaComment = Color("comment")
-    public static let draculaCyan = Color("cyan")
-    public static let draculaGreen = Color("green")
-    public static let draculaOrange = Color("orange")
-    public static let draculaPink = Color("pink")
-    public static let draculaPurple = Color("purple")
-    public static let draculaRed = Color("red")
-    public static let draculaYellow = Color("yellow")
+public extension Color {
+    static let draculaBackground = Color("background")
+    static let draculaCurrent = Color("current")
+    static let draculaForeground = Color("foreground")
+    static let draculaComment = Color("comment")
+    static let draculaCyan = Color("cyan")
+    static let draculaGreen = Color("green")
+    static let draculaOrange = Color("orange")
+    static let draculaPink = Color("pink")
+    static let draculaPurple = Color("purple")
+    static let draculaRed = Color("red")
+    static let draculaYellow = Color("yellow")
 }
