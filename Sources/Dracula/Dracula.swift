b075f40a18229e182d631fc8847fe08ee2c74501
Raphael Cerqueira <raphaelfc@gmail.com>
2021-04-14 20:23:31 -0300

    Resources

--- Sources/Dracula/Dracula.swift
+++ Sources/Dracula/Dracula.swift
@@ -1,16 +1,16 @@
 
-import SwiftUI
+import UIKit
 
-public extension Color {
+public extension UIColor {
     static var draculaBackground = UIColor(named: "background", in: .module, compatibleWith: nil)
-    static let draculaCurrent = Color("current")
-    static let draculaForeground = Color("foreground")
-    static let draculaComment = Color("comment")
-    static let draculaCyan = Color("cyan")
-    static let draculaGreen = Color("green")
-    static let draculaOrange = Color("orange")
-    static let draculaPink = Color("pink")
-    static let draculaPurple = Color("purple")
-    static let draculaRed = Color("red")
-    static let draculaYellow = Color("yellow")
+//    static let draculaCurrent = Color("current")
+//    static let draculaForeground = Color("foreground")
+//    static let draculaComment = Color("comment")
+//    static let draculaCyan = Color("cyan")
+//    static let draculaGreen = Color("green")
+//    static let draculaOrange = Color("orange")
+//    static let draculaPink = Color("pink")
+//    static let draculaPurple = Color("purple")
+//    static let draculaRed = Color("red")
+//    static let draculaYellow = Color("yellow")
 }
